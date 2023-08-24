8e06d6ed198850ed96327c35dbdf9a0f1265f310
poseidon0z <adityaprabhu1290@gmail.com>
2023-08-24 21:54:55 +0530

    Added circular linked list

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## singly linked list output:
 
-![Simply Linked List sample output](image.png)
+![Simply Linked List sample output](Outputs/singly_linked_list.png)
+![Circular Linked List sample output](Outputs/circular_linked_list.png)
---
+++ circular_linked_list.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+struct node
+{
+    int data;
+    struct node *next;
+} *head, *tail, *temp, *temp2;
+
+int main()
+{
+    // Creating circular linked list
+    temp = (struct node *)malloc(sizeof(struct node));
+    printf("Enter first data: ");
+    scanf("%d", &(temp->data));
+    temp->next = temp;
+    head = temp;
+    tail = temp;
+
+    // Adding further elements
+    for (int i = 0; i < 5; i++)
+    {
+        temp2 = (struct node *)malloc(sizeof(struct node));
+        printf("Enter data: ");
+        scanf("%d", &(temp2->data));
+        temp->next = temp2;
+        temp2->next = head;
+        temp = temp2;
+        tail = temp2;
+    }
+
+    // Adding element to beginning
+    temp2 = (struct node *)malloc(sizeof(struct node));
+    temp2->next = head;
+    printf("Enter value to add to beginning: ");
+    scanf("%d", &(temp2->data));
+    head = temp2;
+    tail->next = head;
+
+    // Adding element to middle
+    printf("Enter the position");
+    int pos;
+    scanf("%d", &pos);
+
+    temp = head;
+    for (int i = 0; i < pos - 2; i++)
+    {
+        temp = temp->next;
+    }
+    temp2 = (struct node *)malloc(sizeof(struct node));
+    temp2->next = temp->next;
+    temp->next = temp2;
+    printf("Enter teh value to add to this postion: ");
+    scanf("%d", &(temp2->data));
+
+    // Adding element to end
+    temp2 = (struct node *)malloc(sizeof(struct node));
+    printf("Enter data to add to end: ");
+    scanf("%d", &(temp2->data));
+    tail->next = temp2;
+    tail = temp2;
+    tail->next = head;
+
+    // Printing the final list
+    temp = head;
+    for (int i = 0; i < 9; i++)
+    {
+        printf("%d", temp->data);
+        temp = temp->next;
+    }
+}
