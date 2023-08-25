456bf5cfb3ae9044f1665d78c0192090415763ec
poseidon0z <adityaprabhu1290@gmail.com>
2023-08-25 23:39:31 +0530

    Added deletion in doubly linked list

--- doubly_linked_list.c
+++ doubly_linked_list.c
@@ -48,17 +48,44 @@ int main()
     temp2->prev = temp1;
     temp1->next = temp2;
     temp2->next->prev = temp2;
+
     temp2 = (struct node *)malloc(sizeof(struct node));
     printf("Enter the value to add in the end of the list");
-    scanf("%d", &tail->data);
+    scanf("%d", &temp2->data);
     tail->next = temp2;
     temp2->prev = tail;
     temp2->next = NULL;
     tail = temp2;
+
+    // deleting beginning node
+    temp2 = head->next;
+    temp2->prev = NULL;
+    free(head);
+    head = temp2;
     temp1 = head;
-    for (int i = 0; i < 9; i++)
+    // deleting in the middle
+    printf("Enter the position of element you want to delete");
+    scanf("%d", &pos);
+    temp1 = head;
+    for (int i = 0; i < pos - 2; i++)
+    {
+        temp1 = temp1->next;
+    }
+    temp2 = temp1->next;
+    temp1->next = temp1->next->next;
+    temp1->next->prev = temp1;
+    free(temp2);
+
+    // deleting last element
+    temp1 = tail->prev;
+    temp1->next = NULL;
+    free(tail);
+    tail = temp1;
+    temp1 = head;
+    while (temp1 != tail)
     {
         printf("%d", temp1->data);
         temp1 = temp1->next;
     }
+    printf("%d", tail->data);
 }
