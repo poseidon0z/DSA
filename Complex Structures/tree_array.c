a498cda35710ed98e62989da8fad002157813ac3
poseidon0z <adityaprabhu1290@gmail.com>
2023-12-02 13:27:50 +0530

    Added search in tree array

--- Complex Structures/tree_array.c
+++ Complex Structures/tree_array.c
@@ -3,7 +3,7 @@ int tree[15];
 
 void takeInputPreorder(int root)
 {
-    if (root <= sizeof(tree) / sizeof(int))
+    if (root < sizeof(tree) / sizeof(int))
     {
         scanf("%d", &tree[root]);
         takeInputPreorder(root * 2 + 1);
@@ -12,7 +12,7 @@ void takeInputPreorder(int root)
 }
 void takeInputInorder(int root)
 {
-    if (root <= sizeof(tree) / sizeof(int))
+    if (root < sizeof(tree) / sizeof(int))
     {
         takeInputInorder(root * 2 + 1);
         scanf("%d", &tree[root]);
@@ -22,11 +22,45 @@ void takeInputInorder(int root)
 
 void postOrder(int root)
 {
-    if (root <= sizeof(tree) / sizeof(int))
+    if (root < sizeof(tree) / sizeof(int))
     {
         postOrder(root * 2 + 1);
         postOrder(root * 2 + 2);
         printf("%d ", tree[root]);
+    }
+}
+
+int getIndex(int root, int k)
+{
+    if (root < sizeof(tree) / sizeof(int))
+    {
+        int temp;
+        temp = getIndex(root * 2 + 1, k);
+        if (temp != -1)
+        {
+            return temp;
+        }
+
+        temp = getIndex(root * 2 + 2, k);
+
+        if (temp != -1)
+        {
+            return temp;
+        }
+        printf("%d ", tree[root]);
+        if (tree[root] == k)
+        {
+            printf("Found at %d", root);
+            return root;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        return -1;
     }
 }
 int main()
@@ -34,5 +68,5 @@ int main()
     printf("Give values in Inorder: ");
     takeInputInorder(0);
 
-    postOrder(0);
+    printf("\n%d", getIndex(0, 12));
 }
